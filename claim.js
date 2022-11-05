9bf3b13f6cfd22a60aac72559b17cb7c20ca74d0
sebushi113 <sebushi113@users.noreply.github.com>
2022-11-05 19:30:37 -0400

    make it better

--- claim.js
+++ claim.js
@@ -20,11 +20,9 @@ const cs1a = process.env.cs1a;
 const cs1p = process.env.cs1p;
 const cd3a = process.env.cd3a;
 const cd3p = process.env.cd3p;
-
-const date = "YYYY-MM-DD HH:mm:ss";
-
 const chat_id = process.env.chat_id;
 const chat_id2 = process.env.chat_id2;
+const date = "YYYY-MM-DD HH:mm:ss";
 
 function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
